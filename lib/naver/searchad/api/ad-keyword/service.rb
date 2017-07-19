2df7e51bca43efe05129f604cd53bbc9454679e8
Min.Kim <min.kim@forward3d.com>
2017-07-19 17:47:42 +0100

    Add a method for list keywords

--- lib/naver/searchad/api/ad-keyword/service.rb
+++ lib/naver/searchad/api/ad-keyword/service.rb
@@ -5,34 +5,36 @@ module Naver
         class Service < Naver::Searchad::Api::Core::BaseService
 
           def initialize
-            super('https://api.naver.com/', 'ncc/keywords/')
+            super('https://api.naver.com/', 'ncc/')
           end
 
-          def list_ad_keywords_by_ids(ad_keyword_ids, &block)
-
+          def list_ad_keywords_by_ids(ad_keyword_ids, options: nil, &block)
+            command = make_command(:get, 'keywords', options)
+            command.query['ids'] = ad_keyword_ids.join(',')
+            execute_command(command, &block)
           end
 
-          def list_ad_keywords_by_adgroup_id(adgroup_id, &block)
-
-          end
-
-          def list_ad_keywords_by_label_id(label_id, &block)
-
-          end
-
-          def get_ad_keyword(ad_keyword_id, &block)
+          def list_ad_keywords_by_adgroup_id(adgroup_id, options: nil, &block)
 
           end
 
-          def create_ad_keyword(ad_keyword, &block)
+          def list_ad_keywords_by_label_id(label_id, options: nil, &block)
 
           end
 
-          def update_ad_keyword(ad_keyword, fields, &block)
+          def get_ad_keyword(ad_keyword_id, options: nil, &block)
 
           end
 
-          def delete_ad_keyword(ad_keyword_id, &block)
+          def create_ad_keyword(ad_keyword, options: nil, &block)
+
+          end
+
+          def update_ad_keyword(ad_keyword, fields: nil, options: nil, &block)
+
+          end
+
+          def delete_ad_keyword(ad_keyword_id, options: nil, block)
           end
         end
       end
