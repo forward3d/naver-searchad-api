57f6449687d9891f19d85f5029e64d0d9ec2f6e3
Min.Kim <min.kim@forward3d.com>
2017-08-01 18:15:24 +0100

    Bump version to 0.0.3

--- lib/naver/searchad/api/version.rb
+++ lib/naver/searchad/api/version.rb
@@ -1,7 +1,7 @@
 module Naver
   module Searchad
     module Api
-      VERSION = '0.0.2'
+      VERSION = '0.0.3'
 
       OS_VERSION = begin
         if RUBY_PLATFORM =~ /mswin|win32|mingw|bccwin|cygwin/
