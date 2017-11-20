9da3559c7e0c0fadd86fb9a11eb64e9459f76181
Min.Kim <minsikzzang@gmail.com>
2017-11-20 11:01:24 +0000

    Bump version

--- lib/naver/searchad/api/version.rb
+++ lib/naver/searchad/api/version.rb
@@ -1,7 +1,7 @@
 module Naver
   module Searchad
     module Api
-      VERSION = '0.0.5'
+      VERSION = '0.0.6'
 
       OS_VERSION = begin
         if RUBY_PLATFORM =~ /mswin|win32|mingw|bccwin|cygwin/
