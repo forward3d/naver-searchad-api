e408dcd6c6d497e758ac4c55e5edbcc097f5a417
Evgeni Radev <evgeniradev@gmail.com>
2022-11-28 12:54:54 +0000

    change version to 1.1.0

--- lib/naver/searchad/api/version.rb
+++ lib/naver/searchad/api/version.rb
@@ -1,7 +1,7 @@
 module Naver
   module Searchad
     module Api
-      VERSION = '1.0.1'
+      VERSION = '1.1.0'
 
       OS_VERSION = begin
         if RUBY_PLATFORM =~ /mswin|win32|mingw|bccwin|cygwin/
